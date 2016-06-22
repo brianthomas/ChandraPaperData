65fe08ac2322c3a4e4550ecf9d3df26a42f5e74b
emilyrayfield <emily.m.rayfield@nasa.gov>
2016-06-22 15:13:59 -0400

    Updated keyword parse  Please enter the commit message for your changes. Lines starting

--- bin/parse_keywords.py
+++ bin/parse_keywords.py
@@ -1,13 +1,16 @@
 import json
 import codecs
 
-filename = "data/chandra_all_cat_results.json"
-#filename = "test.json"
+filename = 'chandra_all_cat_results.json'
 with codecs.open(filename,'r',encoding='utf-8') as f:
     chandra_data = json.load(f)
 
-#keywords = chandra_data[0]["keyword"]
-print (str(chandra_data[0]['response']['docs']))
-#print(keywords)
+keywords = []
 
-print ("Finished")
+for i in range((len(chandra_data))):
+    if chandra_data[i]['response']['docs'] != [] and 'keyword' in chandra_data[i]['response']['docs'][0]:
+        keywords.append(chandra_data[i]['response']['docs'][0]['keyword'])
+    else:
+        pass
+
+print(keywords)
