d7e823bfb71844fa03104c7d4c3634c05ca7a278
Brian Thomas <brian.a.thomas@nasa.gov>
2016-08-02 10:28:52 -0400

    interim checking to preserve working code

--- bin/ads2term_dict_model.py
+++ bin/ads2term_dict_model.py
@@ -15,35 +15,15 @@ Created on Jul 12, 2016
 # dictionary
 MIN_TERM_OCCUR = 3
 
-# Rules for processing tokens which we may miss with ordinary
-# processing engine. Rules which match will cause all whitespace
-# to be changed into underscores so that the words will be picked
-# up as a group 
-special_token_patterns = ['\<ASTROBJ\>[\w|\s|\.|\-|\+]+\<\/ASTROBJ\>', 'NGC\s+\d+', ]
-
-# some common formatting patterns we need to clean out
-format_token_replacement_patterns = { ':': '', '-': ' ', '[': '', ']': '',  '(': '',
-                                      ')': '', '\'': '', '=': '', '&': 'and', '\\': '',  
-                                    } 
-# some replacement patterns germane to just Chandra data
-# includes some particular misspellings and duplications
-chandra_token_repl_patterns = { 'H II': 'HII', 'h\s{0,1}ii': 'HII', 'balck': 'black', 
-                                'intergalacic': 'intergalactic', 'submillimetre': 'submillimeter', 
-                                'centre': 'center', 'agn': 'AGN', 'cd': 'CD', 'disc': 'disk'
-                              }
-
 '''
-    # IF its not all uppercase, then not acronyms, so lets stem it
-    if token_list[j] != token_list[j].upper():
-        token_list[j] = nltk.stem.SnowballStemmer('english').stem(token_list[j])
-        
     # add underscore to preserve spacing?? -- Ask Emily about this  
     keyword_new = '_'.join(token_list)
     keyword_new = keyword_new.strip('_')
-    
 '''
 
 import ocio.textmining.extraction as ote
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 class EnglishStemmer (ote.StemmingTool):
     
@@ -61,12 +41,69 @@ class EnglishStemmer (ote.StemmingTool):
 
     def __init__(self): pass
     
-def createTermDictionaryFromAbstracts (jsonfile, output_dict_model, output_processing_rules_model, min_term_occur):
+class AbstractTextProcessor (ote.ProcessTextTool):
+    
+    # Rules for processing tokens which we may miss with ordinary
+    # processing engine. Rules which match will cause all whitespace
+    # to be changed into underscores so that the words will be picked
+    # up as a group 
+    _Special_Token_Patterns = ['\<ASTROBJ\>[\w|\s|\.|\-|\+]+\<\/ASTROBJ\>', 'NGC\s+\d+', ]
+    
+    # some common formatting patterns we need to clean out
+    _Format_Token_Replacement_Patterns = { ':': '', '-': ' ', '[': '', ']': '',  '(': '',
+                                          ')': '', '\'': '', '=': '', '&': 'and', '\\': '',
+                                          '<ASTROBJ>' : '', '</ASTROBJ>' : '', 
+                                          '<SUP>' : '', '</SUP>' : '', 
+                                          '~' : '',
+                                        } 
+    
+    # some replacement patterns germane to just Chandra data
+    # includes some particular misspellings and duplications
+    _Chandra_Token_Repl_Patterns = { 'H II': 'HII', 'h\s{0,1}ii': 'HII', 'balck': 'black', 
+                                    'intergalacic': 'intergalactic', 'submillimetre': 'submillimeter', 
+                                    'centre': 'center', 'agn': 'AGN', 'cd': 'CD', 'disc': 'disk'
+                                  }
+    
+    @staticmethod
+    def process (abstract):
+        import re
+        
+        # initialize processing rules from expected patterns
+        processing_rules = dict( AbstractTextProcessor._Chandra_Token_Repl_Patterns, 
+                                 **AbstractTextProcessor._Format_Token_Replacement_Patterns) 
+    
+        # process it to pull out sources, changing spaces to underscore so we make
+        # sure to capture the term fully
+        for pattern in AbstractTextProcessor._Special_Token_Patterns:
+            results = re.findall(pattern, abstract)
+            replacement_labels = {}
+            if results:
+                for result in results:
+                     
+                    #clean up XML-ish formatting
+                    result = re.sub('\<\/?\w+\>', '', result)
+                     
+                    # store the processing rule for downstream use
+                    # replace spaces with underscore so we ensure tokenization
+                    #processing_rules[result] = result.replace(' ','_')
+                    abstract = abstract.replace(result, result.replace(' ','_'))
+        
+        # apply all of the processing rules now 
+        for rule in processing_rules:
+            abstract = abstract.replace(rule, processing_rules[rule])
+            
+        # finally, trim leading and trailing whitespace
+        # and return
+        return abstract.strip()
+        
+    def __init__(self): pass
+    
+
+def createTermDictionaryFromAbstracts (jsonfile, output_dict_model, min_term_occur):
     
     import pickle
     import json
     import codecs
-    import re
     from gensim.parsing.preprocessing import STOPWORDS
     
     print ("Training using file: "+jsonfile+" with minimum term threshold:"+str(min_term_occur))
@@ -76,10 +113,6 @@ def createTermDictionaryFromAbstracts (jsonfile, output_dict_model, output_proce
     with codecs.open(jsonfile,'r',encoding='utf-8') as f:
         chandra_data = json.load(f)
 
-    
-    # initialize processing rules from expected patterns
-    processing_rules = dict( chandra_token_repl_patterns, **format_token_replacement_patterns) 
-
     # process our corpus
     corpus = []
     for i in range((len(chandra_data))):
@@ -88,39 +121,17 @@ def createTermDictionaryFromAbstracts (jsonfile, output_dict_model, output_proce
             # grab the raw abstract
             abstract = chandra_data[i]['response']['docs'][0]['abstract']
             
-            # process it to pull out sources, changing spaces to underscore so we make
-            # sure to capture the term fully
-            for pattern in special_token_patterns:
-                results = re.findall(pattern, abstract)
-                replacement_labels = {}
-                if results:
-                    for result in results:
-                         
-                        #clean up XML-ish formatting
-                        result = re.sub('\<\/?\w+\>','', result)
-                         
-                        # store the processing rule for downstream use
-                        # replace spaces with underscore so we ensure tokenization
-                        processing_rules[result] = result.replace(' ','_')
-            
-            
-            # apply all of the processing rules now 
-            for rule in processing_rules:
-                abstract = abstract.replace(rule, processing_rules[rule])
-                
+            # collect
             corpus.append(abstract)
             
         else:
             pass
 
-    print (" * Writing processing rules model")
-    with open(output_processing_rules_model, 'wb+') as f:
-        pickle.dump(processing_rules, f)
-
     print (" * Creating trained model from ADS abstract field")
-    dict_model = ote.UnstructuredTextTermExtractor.train(corpus, stop_words=STOPWORDS, 
-                                                           stemming_tool=EnglishStemmer(),
-                                                           min_term_count=min_term_occur)
+    dict_model = ote.UnstructuredTextTermExtractor.train( corpus, stop_words=STOPWORDS, 
+                                                          preprocess_text_tool=AbstractTextProcessor(),
+                                                          stemming_tool=EnglishStemmer(),
+                                                          min_term_count=min_term_occur)
     
     print (" * Writing pickled output to file:"+ output_dict_model)
     with open(output_dict_model, 'wb+') as f:
@@ -137,7 +148,6 @@ if __name__ == '__main__':
     ap.add_argument('-i', '--input', type=str, help='JSON formatted file to pull from')
     ap.add_argument('-m', '--output_dict_model', type=str, help='Output model file to write to (in pickled form)')
     ap.add_argument('-mt', '--min_term_threshold', type=int, default=MIN_TERM_OCCUR, help='Minimum number of times term must occur in training to be included in the model dictionary ')
-    ap.add_argument('-p', '--output_processing_rules_model', type=str, help='Output processing rules model file to write to (in pickled form)')
    
     # parse argv
     opts = ap.parse_args()
@@ -152,12 +162,7 @@ if __name__ == '__main__':
         ap.print_usage()
         exit()
     
-    if not opts.output_processing_rules_model:
-        print ("the --output_processing_rules_model <file> parameter must be specified")
-        ap.print_usage()
-        exit()
-
-    createTermDictionaryFromAbstracts(opts.input, opts.output_dict_model, opts.output_processing_rules_model, opts.min_term_threshold)
+    createTermDictionaryFromAbstracts(opts.input, opts.output_dict_model, opts.min_term_threshold)
     
     
     
